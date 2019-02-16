cc31bcc5a74f4905d84a29ffa8e1f4818259fe79
PPonta <masatohappys2@gmail.com>
2019-02-16 17:30:57 +0900

    mainページ更新＠伊藤

--- main.php
+++ main.php
@@ -11,19 +11,75 @@ $user_userid = '@hamuchan86';
 $user_mypage_url = 'mypage.php';
 ?>
 
-<!-- (テスト用)SQLを実行して検索結果を表示(テスト用) -->
+<!-- SQLを実行して検索結果を表示 -->
 <?php
-	for($i=0; $i<5; $i++)
-	{
-    	//ルーム一覧の情報
-		$room_hosticon_url[] = 'http://www.geocities.jp/masatohappys/kumotennsi/hamugaku.png';
-		$room_hostname[] = 'ハムちゃん';
-		$room_hostid[] = '@hamuchan86';
-		$room_hostroom_url[] = 'room.php';
-		$room_roomurl[] = 'mypage.php';
-		$room_title[] = 'ハムの英会話教室';
-		$room_comment[] = 'なんでも教えます。';
-	}
+    require_once('mysql_connect.php');//外部ファイルを使う
+	//URLからGETパラメータを取得
+    if(isset($_GET['keyword'])){
+        $keyword = $_GET['keyword'];
+        if(isset($_GET['offset'])){
+            $offset = $_GET['offset'];
+        } else {
+            $offset = 0;
+        }
+        try {
+                //データベースに接続
+                $pdo = connectDB();//外部ファイルのメソッド
+                //SQL文を用意(roomテーブルから、検索ワードと一致するチャットルーム情報を取得する)
+                $sql = "SELECT * FROM room WHERE title like '%:keyword%' OR name like '%:keyword%' LIMIT 5 OFFSET :offset";
+                //SQL文をいったんpStmtクラス変数に入れる
+                $pStmt = $pdo->prepare($sql);
+                //変数に値を入れてSQLを実行
+                $pStmt->bindValue(':keyword', $keyword, PDO::PARAM_STR);
+                $pStmt->bindValue(':offset', $offset, PDO::PARAM_STR);
+                $pStmt->execute();
+                //結果を配列に格納
+                $roomcount = 0;
+                foreach( $pStmt as $value )
+                {
+                    $roomcount++;
+                    //ルーム一覧の情報
+                    $room_hosticon_url[] = $value[hosticonurl];//ルームホストのアイコン
+                    $room_hostname[] = $value[hostname];//ルームホストの名前(ハムちゃん)
+                    $room_hostid[] = $value[hostid];//ルームホストのID名(@hamuchan86)
+                    $room_hostroom_url[] = $value[hosturl];//ホストのマイページURL
+                    $room_roomurl[] = $value[roomurl];//チャットルームのURL
+                    $room_title[] = $value[title];//ルーム名
+                    $room_comment[] = $value[comment];//ルーム説明欄
+                }
+        } catch(PDOException $e) {
+                echo $e->getMessage();
+                die();
+        }
+    } else {
+        try {
+                //データベースに接続
+                $pdo = connectDB();//外部ファイルのメソッド
+                //SQL文を用意(roomテーブルから、検索ワードと一致するチャットルーム情報を取得する)
+                $sql = "SELECT * FROM room LIMIT 5 OFFSET 0";
+                //SQL文をいったんpStmtクラス変数に入れる
+                $pStmt = $pdo->prepare($sql);
+                //SQLを実行
+                $pStmt->execute();
+                //結果を配列に格納
+                $roomcount = 0;
+                foreach( $pStmt as $value )
+                {
+                    $roomcount++;
+                	//ルーム一覧の情報
+    				$room_hosticon_url[] = $value[hosticonurl];//ルームホストのアイコン
+    				$room_hostname[] = $value[hostname];//ルームホストの名前(ハムちゃん)
+    				$room_hostid[] = $value[hostid];//ルームホストのID名(@hamuchan86)
+    				$room_hostroom_url[] = $value[hosturl];//ホストのマイページURL
+    				$room_roomurl[] = $value[roomurl];//チャットルームのURL
+    				$room_title[] = $value[title];//ルーム名
+    				$room_comment[] = $value[comment];//ルーム説明欄
+                }
+        } catch(PDOException $e) {
+                echo $e->getMessage();
+                die();
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -100,7 +156,7 @@ $user_mypage_url = 'mypage.php';
 							</div>
 						</form>
 					</div>
-					<?php for($i=0; $i<5; $i++){ ?>
+					<?php for($i=0; $i<$roomcount; $i++){ ?>
 				 		<div class="maincontents"><!-- メインコンテンツのブロック -->
 				 			<div class="roombrock1">
 		 						<?php
