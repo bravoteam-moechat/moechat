9ab27f4fa58e2b5f21358047e71d7a56d9cbe71c
kyosukesa10 <kyosukesa10@gmail.com>
2019-02-16 17:00:38 +0900

    いらないファイルの削除、login.phpのエラー表示をincludeで追加＠佐藤

--- login.php
+++ login.php
@@ -4,11 +4,7 @@ require_once('mysql_connect.php');
 $pdo = connectDB();
 
 session_start();
-//POSTのvalidate
-// if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-//   echo '入力された値が不正です。';
-//   return false;
-// }
+
 //DB内でPOSTされたメールアドレスを検索
 try {
   $stmt = $pdo->prepare('select * from moechat where username = ?');
@@ -19,18 +15,19 @@ try {
 }
 //emailがDB内に存在しているか確認
 if (!isset($row['username'])) {
-  echo 'ユーザーネーム又はパスワードが間違っています。';
+  echo "<script>alert('ユーザーネーム又はパスワードが間違っています。');</script>";
+  include 'index.html';
+  exit();
   return false;
 }
 //パスワード確認後sessionにメールアドレスを渡す
 if($_POST['password'] == $row['password']){
   $_SESSION['user_id']=$row['id'];
-// if (password_verify($_POST['password'], $row['password'])) {
-//   session_regenerate_id(true); //session_idを新しく生成し、置き換える
-//   $_SESSION['EMAIL'] = $row['email'];
   header('Location: main.html');
     exit();
 } else {
-  echo 'メールアドレス又はパスワードが間違っています。';
+  echo "<script>alert('ユーザーネーム又はパスワードが間違っています。');</script>";
+  include 'index.html';
+  exit();
   return false;
 }
